644a825ed773f7578fff775be887e5729a9fc695
SCL <18680869068s@gmail.com>
2018-12-31 10:26:45 +0800

    suncanlong   新建项目

---
+++ source/source/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  source
+//
+//  Created by can on 2018/12/31.
+//  Copyright © 2018年 can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ source/source/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  source
+//
+//  Created by can on 2018/12/31.
+//  Copyright © 2018年 can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

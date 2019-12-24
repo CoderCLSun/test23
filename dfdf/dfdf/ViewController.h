50c44b0966cfdfe3a9c5574814f085ed2f7096b6
SCL <18680869068s@gmail.com>
2019-12-24 10:35:45 +0800

    suncanlong大幅度发

--- dfdf/dfdf/ViewController.h
+++ dfdf/dfdf/ViewController.h
@@ -1,16 +1,14 @@
 //
 //  ViewController.h
-//  source
+//  dfdf
 //
-//  Created by can on 2018/12/31.
-//  Copyright © 2018年 can. All rights reserved.
+//  Created by can on 2019/4/20.
+//  Copyright © 2019年 can. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-
 
 
 @end
---
+++ dfdf/dfdf/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dfdf
+//
+//  Created by can on 2019/4/20.
+//  Copyright © 2019年 can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
